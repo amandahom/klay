5447535bfc907745ec08724880e920c67ac12c78
Mandy Hom <amandakhom@gmail.com>
2022-01-04 19:43:53 -0800

    Finalizing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 const Home: NextPage = () => {
   const [gameTime, setGameTime] = useState(false);
@@ -8,6 +10,7 @@ const Home: NextPage = () => {
   const [hour, setHour] = useState(0);
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
+  const { width, height } = useWindowSize();
 
   useEffect(() => {
     const targetDate = new Date("01/09/2022 17:30:00");
@@ -60,64 +63,73 @@ const Home: NextPage = () => {
             opacity: 1;
           }
         `}</style>
-        <div className="pt-32">
+        <div className="pt-36">
           <div className="text-center">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <h1 className="text-4xl tracking-tight font-extrabold text-blue-900 sm:text-5xl md:text-6xl">
               <span className="block xl:inline">Klay Thompson </span>
-              <span className="block text-white xl:inline">Countdown</span>
+              <span className="block text-yellow-500  xl:inline">
+                Countdown
+              </span>
             </h1>
-            <p className="mt-3 text-base text-white sm:mt-5 sm:text-lg">
-              Because I can't wait and I'm bored.
-            </p>
           </div>
 
           {gameTime ? (
             <>
-              <h1>Game Time.</h1>
-              {/* <video autoPlay loop muted> */}
-              {/* <source src="/party.mp4" /> */}
-              {/* </video> */}
+              <div
+                className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none"
+                id="modal-id"
+              >
+                <Confetti width={width} height={height} />
+                <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
+                <div className="w-full max-w-lg p-4 sm:p-10 relative mx-4 md:mx-auto my-auto rounded-xl shadow-lg bg-white">
+                  <div className="">
+                    <div className="text-center p-10 flex-auto justify-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-32 h-32 flex items-center text-red-500 mx-auto"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                        />
+                      </svg>
+                      <h2 className="text-5xl font-bold py-4 ">
+                        KLAY IS BACK!
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </>
           ) : (
             <>
-              <div className="text-center text-white pt-48">
-                <div className="inline-block p-10">
-                  <p className="text-7xl inline-block">{day}</p>
-                  <p className="text-base">Days</p>
+              <div className="text-center text-white pt-20 md:pt-16">
+                <div className="inline-block p-4 md:p-10">
+                  <p className="text-5xl md:text-8xl inline-block">{day}</p>
+                  <p className="text-sm md:text-xl">Days</p>
                 </div>
-                <span className="text-7xl">:</span>
-                <div className="inline-block p-10">
-                  <p className="text-7xl inline-block">{hour}</p>
-                  <p className="text-base">Hours</p>
+                <div className="inline-block p-4 md:p-10">
+                  <p className="text-5xl md:text-8xl inline-block">{hour}</p>
+                  <p className="text-sm md:text-xl">Hours</p>
                 </div>
-                <span className="text-7xl">:</span>
-                <div className="inline-block p-10">
-                  <p className="text-7xl inline-block">{minute}</p>
-                  <p className="text-base">Minutes</p>
+                <div className="inline-block p-4 md:p-10">
+                  <p className="text-5xl md:text-8xl inline-block">{minute}</p>
+                  <p className="text-sm md:text-xl">Minutes</p>
                 </div>
-                <span className="text-7xl">:</span>
-                <div className="inline-block p-10">
-                  <p className="text-7xl inline-block">{second}</p>
-                  <p className="text-base">Seconds</p>
+                <div className="inline-block p-4 md:p-10">
+                  <p className="text-5xl md:text-8xl inline-block">{second}</p>
+                  <p className="text-sm md:text-xl">Seconds</p>
                 </div>
               </div>
             </>
           )}
         </div>
       </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
     </div>
   );
 };
